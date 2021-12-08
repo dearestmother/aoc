0358683dfb312e910861a6cc86850e0324a3d65f
John Goewert <goewerj1@hill-rom.com>
2021-12-08 08:53:31 -0500

    Completed Day 8

--- 2021/day8.py
+++ 2021/day8.py
@@ -1,5 +1,6 @@
 from aocd.models import Puzzle
 import numpy as np
+from collections import Counter
 
 def part1(input):
     digitcounts = np.zeros(10)
@@ -19,9 +20,97 @@ def part1(input):
 
     print ("Part 1: {}".format(int(np.sum(digitcounts))))
 
-def part2(input):
 
-    print ("Part 2: {}".format(0))
+def decode_dictionary(inp):
+    digits = {}
+    five_len, six_len = [], []
+
+    for digit in inp:
+        if len(digit) == 2:
+            digits["1"] = set(digit)
+        elif len(digit) == 4:
+            digits["4"] = set(digit)
+        elif len(digit) == 3:
+            digits["7"] = set(digit)
+        elif len(digit) == 7:
+            digits["8"] = set(digit)
+        elif len(digit) == 5:
+            five_len.append(set(digit))
+        elif len(digit) == 6:
+            six_len.append(set(digit))
+
+    for digit in five_len:
+        # 2, 3, 5
+        if len(digit & digits["1"]) and len(digit & digits["4"]) == 2:
+            digits["2"] = digit
+        elif len(digit & digits["7"]) == 3:
+            digits["3"] = digit
+        elif len(digit & digits["1"]) == 1 and len(digit & digits["4"]) == 3:
+            digits["5"] = digit
+
+    for digit in six_len:
+        # 6, 9, 0
+        if len(digit & digits["1"]) == 1:
+            digits["6"] = digit
+        elif len(digit & digits["4"]) == 4:
+            digits["9"] = digit
+        elif len(digit & digits["4"]) == 3:
+            digits["0"] = digit
+
+    decoded = {"".join(sorted(v)): k for k, v in digits.items()}
+    return decoded
+
+
+def part2(input):
+    problem = []
+    for line in input:
+        inp, out = line.split("|")
+        inp, out = inp.split(), out.split()
+        problem.append((inp, out))
+    
+    sum = 0
+    for inp, out in problem:
+        decoded = decode_dictionary(inp)
+        result = []
+        for digit in out:
+            digit = "".join(sorted(digit))
+            result.append(decoded[digit])
+        sum += int("".join(result))
+    print ("Part 2: {}".format(sum))
+
+
+    #digits = ['','','','','','','','','']
+#
+    #for line in input:
+#
+    #    # Find the ones we know
+    #    values = line.split("|")[0].split(" ")
+    #    for value in values:
+    #        match len(value):
+    #            case 2:
+    #                digits[1] = value
+    #            case 3:
+    #                digits[3] = value
+    #            case 4:
+    #                digits[4] = value
+    #            case 7:
+    #                digits[8] = value
+    #    for value in values:
+    #        if len(value) == 6:
+    #            # 0, 6, 9
+    #            for
+#
+    ## 0: left from 6: and 9:
+    ## 2: left from 3: and 5:
+    ## 3: (Candidate - 1) -> 3 segments left
+    ## 5: (candidate - 4) -> 2 segments left
+    ## 6: (8 - candidate) intersect 1
+    ## 9: (Candidate - 4) -> 2 segments left
+    #part_two = 0
+    #for line in input:
+    #    p = process_line(line)
+    #    part_two += int("".join([str(x) for x in p]))
+    #
 
 
 if __name__ == "__main__":
@@ -39,6 +128,6 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |fgae cfgab fg bagce"""
 
     #part1(example.splitlines())
-    #part1(puzzle.input_data.splitlines())
-    part2(example.splitlines())
-    #part2(puzzle.input_data.splitlines())
+    part1(puzzle.input_data.splitlines())
+    #part2(example.splitlines())
+    part2(puzzle.input_data.splitlines())
